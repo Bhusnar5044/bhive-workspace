3dbfa89ef920b0d9d11f25d9cedd10936e392bfb
Pradip Bhusnar <bhusnarpradip@gmail.com>
2024-08-30 04:20:51 +0530

    PB | added home content

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,118 @@
+import { useEffect, useState } from 'react';
+
+import CardChip from '@/components/common/CardChip';
+import SpaceOverviewCard from '@/components/common/SpaceOverviewCard';
+import type { Location } from '@/constants/commonTypes';
+
 const Home = () => {
+  const [data, setData] = useState<Location[]>([]);
+  const getData = () => {
+    fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((myJson) => setData(myJson));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
-    <div className="flex min-h-[60vh] flex-col items-center justify-center gap-8 text-center">
-      <div className="grid gap-2.5">
-        <h1 className="bg-gradient-to-br from-gray-200 to-teal-700 bg-clip-text text-2xl font-bold text-transparent md:text-3xl">
-          vite-react-tailwind-starter
-        </h1>
-        <p className="text-xs text-gray-500 md:text-sm">
-          This is a vite react template with TailwindCSS and TypeScript setup.
-        </p>
-      </div>
-    </div>
+    <>
+      <section className="mb-5">
+        <div className="justify-betweenmb-4 flex">
+          <h2>Why Choose us?</h2>
+          <img
+            src="assets/icons/arrow.svg"
+            alt="arrow icon"
+            className="lg:hidden"
+          />
+        </div>
+        <div className="mx-auto grid grid-cols-2 gap-2 lg:grid-cols-4 lg:gap-0">
+          <CardChip
+            title="Community Events"
+            iconUrl="/assets/icons/calenderEvent.svg"
+            className="shadow shadow-white lg:border-b lg:border-r lg:border-white"
+          />
+          <CardChip
+            title="Gym Facilities"
+            iconUrl="/assets/icons/gymIcon.svg"
+            className="shadow shadow-white lg:border-b lg:border-r lg:border-white"
+          />
+          <CardChip
+            title="High-Speed WiFi"
+            iconUrl="/assets/icons/wifi.svg"
+            className="shadow shadow-white lg:border-b lg:border-r lg:border-white"
+          />
+          <CardChip
+            title="Cafe & Tea Bar"
+            iconUrl="/assets/icons/teaCup.svg"
+            className="shadow shadow-white lg:border-b lg:border-white"
+          />
+          <CardChip
+            title="Affordable"
+            iconUrl="/assets/icons/rupee.svg"
+            className="shadow shadow-white lg:border-r lg:border-white"
+          />
+          <CardChip
+            title="Comfort Lounges"
+            iconUrl="/assets/icons/lounges.svg"
+            className="shadow shadow-white lg:border-r lg:border-white"
+          />
+          <CardChip
+            title="Quick Booking"
+            iconUrl="/assets/icons/clock.svg"
+            className="shadow shadow-white lg:border-r lg:border-white"
+          />
+          <CardChip title="Sports Area" iconUrl="/assets/icons/sport.svg" />
+        </div>
+      </section>
+      <section className="mb-5">
+        <div className="mb-4 flex justify-between">
+          <h2>Our Space Overview</h2>
+          <img
+            src="assets/icons/arrow.svg"
+            alt="arrow icon"
+            className="lg:hidden"
+          />
+        </div>
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          {data?.map((item) => (
+            <SpaceOverviewCard key={item.id} spaceOverviewDetails={item} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <div className="mb-4 flex justify-between">
+          <h2>Download our app now</h2>
+          <img
+            src="assets/icons/arrow.svg"
+            alt="arrow icon"
+            className="lg:hidden"
+          />
+        </div>
+        <div className="flex">
+          <img
+            src="/assets/download-app-banner.svg"
+            alt="download app banner"
+          />
+          <div>
+            <p className="hidden text-textSecondary lg:block">
+              Boost your productivity with the BHIVE Workspace app. Elevate your
+              workspace, collaborate efficiently, and unlock exclusive perks.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
