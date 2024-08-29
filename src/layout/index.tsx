4ef1296cca0b82c590627fb1d0663f9c3dadf27e
Pradip Bhusnar <bhusnarpradip@gmail.com>
2024-08-30 04:20:17 +0530

    PB | updated layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -14,9 +14,22 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <ThemeProvider>
       <Meta />
-      <div className="flex min-h-screen flex-col dark:bg-black dark:text-white">
+      <div className="dark:bg-black flex min-h-screen flex-col bg-lightGray text-textMain dark:text-white">
         <Header />
-        <main className="wrapper">{children}</main>
+        <section className="mb-5 flex flex-wrap-reverse">
+          <div className="flex w-full items-center justify-start bg-banner px-4 lg:w-2/3 lg:px-0">
+            <h1 className="text-2xl text-textMain lg:text-4xl">
+              Host your meeting with world-class amenities. Starting at{' '}
+              <span className="text-primary2">₹199/-!</span>
+            </h1>
+          </div>
+          <img
+            src="/assets/coworkBanner.svg"
+            alt="coworkBanner"
+            className="w-full lg:w-1/3"
+          />
+        </section>
+        <main className="mx-auto max-w-screen-xl px-4 lg:px-0">{children}</main>
         <Footer />
       </div>
     </ThemeProvider>
